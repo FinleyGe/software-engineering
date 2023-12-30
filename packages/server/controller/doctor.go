1c16f65d641423f7e652ee56a1526388ae15b1b0
Finley Ge <m13203533462@163.com>
2023-12-30 22:25:46 +0800

    fix(server): fix doctor login

--- packages/server/controller/doctor.go
+++ packages/server/controller/doctor.go
@@ -26,12 +26,29 @@ func DoctorLogin(c *gin.Context) {
 			ResponseServerError(c)
 		} else {
 			if Config.Dev {
-				c.SetCookie("token", token, 3600, "/", "localhost", false, true)
+				c.SetCookie("token", token, 3600, "/", "localhost", false, false)
 			} else {
-				c.SetCookie("token", token, 3600, Config.Server.ApiRoute, Config.Server.Domain, false, true)
+				c.SetCookie("token", token, 3600, Config.Server.ApiRoute, Config.Server.Domain, false, false)
 			}
 			ResponseOK(c)
 		}
 	}
-	ResponseBadRequest(c)
 }
+
+// func AddPatient(c *gin.Context) {
+// 	request := struct {
+// 		Name string `json:"name"`
+// 	}{}
+//
+// 	patient := model.Patient{
+// 		Name:     request.Name,
+// 		Password: GetDefaultPassword(),
+// 	}
+//
+// 	c.BindJSON(&patient)
+// 	if patient.Add() {
+// 		ResponseOK(c)
+// 	} else {
+// 		ResponseServerError(c)
+// 	}
+// }
