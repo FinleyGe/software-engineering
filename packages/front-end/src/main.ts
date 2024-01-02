f776f4eda3516566899a2071cfeb09da3f510b59
Finley Ge <m13203533462@163.com>
2024-01-02 14:12:27 +0800

    chore(*): router

--- packages/front-end/src/main.ts
+++ packages/front-end/src/main.ts
@@ -1,12 +1,12 @@
-import App from "./App.vue";
-import VueCookies from "vue-cookies";
+import Cookies from "vue-cookies";
 import { createApp } from "vue";
 import { createPinia } from "pinia";
+import App from "./App.vue";
 import router from "@/router";
 import "./tailwind.css";
 
 const app = createApp(App);
 app.use(createPinia());
 app.use(router);
-app.use(VueCookies);
+app.use(Cookies);
 app.mount("#app");
--- packages/front-end/src/router/index.ts
+++ packages/front-end/src/router/index.ts
@@ -5,11 +5,57 @@ const routes = <RouteRecordRaw[]>[
   {
     path: "/",
     name: "Home",
-    component: () => import("@views/Home.vue")
+    component: () => import("@views/Home.vue"),
+    meta:{
+      title:"医院管理系统 | 首页"
+    }
+  },
+  {
+    path: "/admin",
+    children: [
+      {
+        path: "patient",
+        name: "AdminPatient",
+        component: () => import("@views/manage/Patient.vue"),
+        meta:{
+          title:"医院管理系统 | 患者管理"
+        }
+      },
+      {
+        path: "doctor",
+        name: "AdminDoctor",
+        component: () => import("@views/manage/Doctor.vue"),
+        meta: {
+          title: "医院管理系统 | 医生管理"
+        }
+      },
+      {
+        path: "department",
+        name: "AdminDepartment",
+        component: () => import("@views/manage/Department.vue"),
+        meta: {
+          title: "医院管理系统 | 科室管理"
+        }
+      },
+      {
+        path: "bed",
+        name: "AdminBed",
+        component: () => import("@views/manage/Bed.vue"),
+        meta: {
+          title: "医院管理系统 | 床位管理"
+        }
+      }
+    ]
   }
 ];
 
-export default createRouter({
+const router =  createRouter({
   history: createWebHistory(),
   routes
 });
+
+router.beforeEach((to) => {
+  document.title = to.meta.title as string;
+}
+);
+export default router;
